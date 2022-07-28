6264420a0668769cdd942216c3fd44733825f43e
Jenny Peers <jcmansley-peers@hotmail.com>
2022-07-28 12:49:03 +0100

    Added badges to pop up on clicked buttons

--- Dog.js
+++ Dog.js
@@ -11,15 +11,18 @@ class Dog {
   getDogHtml() {
     const { name, avatar, age, bio } = this;
     return `
-            <div class="feature-suggestion">
-                <div class="dog-info">
-                    <h4> ${name}, ${age} </h4>
-                    <div class="dog-bio">
-                        ${bio}
-                    </div>
+        <img class="like-badge" src="images/badge-like.png" alt=" like logo" />
+        <img class="nope-badge" src="images/badge-nope.png" alt=" nope logo" />
+        <div class="feature-suggestion">
+            <div class="dog-info">
+                <h4> ${name}, ${age} </h4>
+                <div class="dog-bio">
+                    ${bio}
                 </div>
-                <img class="dog-img" src="${avatar}">
-            </div>`;
+            </div>
+            <img class="dog-img" src="${avatar}">
+        </div>
+    `;
   }
 }
 
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import Dog from "./Dog.js";
 import dogsData from "./data.js";
+
 let currentDogIndex = 0;
 let currentDog = new Dog(dogsData[currentDogIndex]);
 document.getElementById("accept-button").addEventListener("click", yes);
+document.getElementById("reject-button").addEventListener("click", no);
 
 render();
 
@@ -17,6 +19,15 @@ function getNewDog() {
 }
 
 function yes() {
+  const likeBadge = document.getElementsByClassName("like-badge");
+  likeBadge.classList.add("like-badge-clicked");
   currentDog.setMatchStatus(true);
-  getNewDog();
+  setTimeout(getNewDog, 2000);
+}
+
+function no() {
+  const nopeBadge = document.getElementsByClassName("nope-badge")[0];
+  nopeBadge.classList.add("nope-badge-clicked");
+  currentDog.setMatchStatus(false);
+  setTimeout(getNewDog, 2000);
 }
