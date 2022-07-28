8b4eca682d0214dcfdcc64cd41c618430e55a270
Jenny Peers <jcmansley-peers@hotmail.com>
2022-07-28 13:12:46 +0100

    Added and end alert

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import Dog from "./Dog.js";
-import dogsData from "./data.js";
+import { dogs } from "./data.js";
 
 let currentDogIndex = 0;
-let currentDog = new Dog(dogsData[currentDogIndex]);
+let currentDog = new Dog(dogs[currentDogIndex]);
 document.getElementById("accept-button").addEventListener("click", yes);
 document.getElementById("reject-button").addEventListener("click", no);
 
@@ -13,21 +13,24 @@ function render() {
 }
 
 function getNewDog() {
+  if (currentDogIndex === dogs.length - 1) {
+    return alert(" There are no more dogs. Come back again soon! Woof!");
+  }
   currentDogIndex += 1;
-  currentDog = new Dog(dogsData[currentDogIndex]);
+  currentDog = new Dog(dogs[currentDogIndex]);
   render();
 }
 
 function yes() {
-  const likeBadge = document.getElementsByClassName("like-badge");
+  const likeBadge = document.getElementsByClassName("like-badge")[0];
   likeBadge.classList.add("like-badge-clicked");
   currentDog.setMatchStatus(true);
-  setTimeout(getNewDog, 2000);
+  setTimeout(getNewDog, 1000);
 }
 
 function no() {
   const nopeBadge = document.getElementsByClassName("nope-badge")[0];
   nopeBadge.classList.add("nope-badge-clicked");
   currentDog.setMatchStatus(false);
-  setTimeout(getNewDog, 2000);
+  setTimeout(getNewDog, 1000);
 }
