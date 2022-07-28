4b70fe6c275a43665c1ac526c55011ace4f89b30
Jenny Peers <jcmansley-peers@hotmail.com>
2022-07-28 13:26:12 +0100

    disabled the opposite button from being clicked

--- index.js
+++ index.js
@@ -16,21 +16,27 @@ function getNewDog() {
   if (currentDogIndex === dogs.length - 1) {
     return alert(" There are no more dogs. Come back again soon! Woof!");
   }
+
+  document.getElementById("reject-button").disabled = false;
+  document.getElementById("accept-button").disabled = false;
+
   currentDogIndex += 1;
   currentDog = new Dog(dogs[currentDogIndex]);
   render();
 }
 
 function yes() {
+  document.getElementById("reject-button").disabled = true;
   const likeBadge = document.getElementsByClassName("like-badge")[0];
   likeBadge.classList.add("like-badge-clicked");
   currentDog.setMatchStatus(true);
-  setTimeout(getNewDog, 1000);
+  setTimeout(getNewDog, 2000);
 }
 
 function no() {
+  document.getElementById("accept-button").disabled = true;
   const nopeBadge = document.getElementsByClassName("nope-badge")[0];
   nopeBadge.classList.add("nope-badge-clicked");
   currentDog.setMatchStatus(false);
-  setTimeout(getNewDog, 1000);
+  setTimeout(getNewDog, 2000);
 }
